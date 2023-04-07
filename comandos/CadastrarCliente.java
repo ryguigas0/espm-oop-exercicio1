d206e430eaf5273a99d8f87291c2c51bc81466b6
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-04-07 18:18:57 -0300

    Implemented CadastrarCliente

--- comandos/CadastrarCliente.java
+++ comandos/CadastrarCliente.java
@@ -2,6 +2,7 @@ package comandos;
 
 import java.util.ArrayList;
 
+import models.Cliente;
 import models.Comando;
 import models.Pessoa;
 
@@ -9,8 +10,16 @@ public class CadastrarCliente extends Comando {
 
     @Override
     public void executar(ArrayList<Pessoa> database) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'executar'");
+        String nomeCliente = Auxiliar.dialogoEscrever("Digite o nome do cliente");
+
+        String cpfCliente = Auxiliar.dialogoEscrever("Digite o CPF do cliente");
+
+        double valorDividaCliente = Double
+                .parseDouble(Auxiliar.dialogoEscrever("Digite o valor da divída do cliente").replace(",", "."));
+
+        Cliente newCliente = new Cliente(nomeCliente, cpfCliente, valorDividaCliente);
+
+        database.add(newCliente);
     }
 
 }
