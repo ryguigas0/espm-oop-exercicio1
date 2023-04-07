7373fd51567d00295f4ccbfe369b4a1aae3ea8bf
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-04-07 18:40:20 -0300

    Implemented ListarEmpregados and ListarClientes

--- comandos/ListarClientes.java
+++ comandos/ListarClientes.java
@@ -2,15 +2,28 @@ package comandos;
 
 import java.util.ArrayList;
 
+import models.Cliente;
 import models.Comando;
 import models.Pessoa;
 
-public class ListarClientes extends Comando{
+public class ListarClientes extends Comando {
 
     @Override
     public void executar(ArrayList<Pessoa> database) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'executar'");
+        String output = "";
+
+        for (Pessoa pessoa : database) {
+            if (pessoa instanceof Cliente cliente) {
+                output += cliente + "\n";
+            }
+        }
+
+        if (output.equals("")) {
+            Auxiliar.dialogoMensagem("Clientes não encontrados!");
+        } else {
+            output = "Clientes encontrados:\n" + output;
+            Auxiliar.dialogoMensagem(output);
+        }
     }
 
 }
--- comandos/ListarEmpregados.java
+++ comandos/ListarEmpregados.java
@@ -3,14 +3,27 @@ package comandos;
 import java.util.ArrayList;
 
 import models.Comando;
+import models.Empregado;
 import models.Pessoa;
 
-public class ListarEmpregados extends Comando{
+public class ListarEmpregados extends Comando {
 
     @Override
     public void executar(ArrayList<Pessoa> database) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'executar'");
+        String output = "";
+
+        for (Pessoa pessoa : database) {
+            if (pessoa instanceof Empregado empregado) {
+                output += empregado + "\n";
+            }
+        }
+
+        if (output.equals("")) {
+            Auxiliar.dialogoMensagem("Empregados não encontrados!");
+        } else {
+            output = "Empregados encontrados:\n" + output;
+            Auxiliar.dialogoMensagem(output);
+        }
     }
 
 }
